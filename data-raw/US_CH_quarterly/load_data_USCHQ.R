3d83f57f51f7703f819d99bc575fc3a71fe5f4f8
jrenne <jprenne@gmail.com>
2025-10-28 23:21:42 +0100

    update

--- data-raw/US_CH_quarterly/load_data_USCHQ.R
+++ data-raw/US_CH_quarterly/load_data_USCHQ.R
@@ -1,5 +1,8 @@
 
 US_CH_quarterly <- read.csv2("data-raw/US_CH_quarterly/US_CH_quarterly.csv")
 
+US_CH_quarterly$observation_date <- as.Date(US_CH_quarterly$observation_date)
+names(US_CH_quarterly)[1] <- "date"
+
 save(US_CH_quarterly,file="data/US_CH_quarterly.rda")
 
