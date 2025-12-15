4907adfbe6566df074beac4e45c054625989df22
jrenne <jprenne@gmail.com>
2025-12-15 09:23:31 +0100

    update

--- R/various_proc_ICA.R
+++ R/various_proc_ICA.R
@@ -70,38 +70,6 @@ estim.SVAR.ICA <- function(endog.var,distri,p){
 }
 
 
-#' @export
-simul.distri <- function(distri,nb.sim){
-  # Simulation of independent shocks
-  eps <- NULL
-  for(i in 1:length(distri$type)){
-    if(distri$type[i]=="gaussian"){
-      eps.i <- rnorm(nb.sim)
-    }else if(distri$type[i]=="mixt.gaussian"){
-      p <- distri$p[i]
-      mu.1 <- distri$mu[i]
-      sigma.1 <- distri$sigma[i]
-      mu.2 <- - p/(1-p)*mu.1
-      sigma.2 <- sqrt( 1/(1-p) * (1 - p*sigma.1^2 - p/(1-p)*mu.1^2) )
-      B <- (runif(nb.sim)<p)*1
-      eps.i <- B*rnorm(nb.sim,mean = mu.1,sd = sigma.1) +
-        (1-B)*rnorm(nb.sim,mean = mu.2,sd = sigma.2)
-    }else if(distri$type[i]=="student"){
-      nu <- distri$df[i]
-      eps.i <- rt(nb.sim,df = nu)/sqrt(nu/(nu-2))
-    }else if(distri$type[i]=="laplace"){
-      U <- runif(nb.sim) - .5
-      b <- 1/sqrt(2)
-      eps.i <- - b * sign(U) * log(1 - 2 * abs(U))
-    }else if(distri$type[i]=="hyper.sec"){
-      U <- runif(nb.sim)
-      eps.i <- 2/pi * log(tan(pi/2*U))
-    }
-    eps <- cbind(eps,eps.i)
-  }
-  return(eps)
-}
-
 
 #' @keywords internal
 #' @noRd
--- R/various_proc_basics.R
+++ R/various_proc_basics.R
@@ -330,29 +330,33 @@ make.PHI <- function(Phi){
   return(PHI)
 }
 
-simul.distri_OLD <- function(distri,nb.sim,basic.drawings=NaN){
+
+#' @export
+simul.distri <- function(distri,nb.sim){
   # Simulation of independent shocks
   eps <- NULL
-  nb.var <- length(distri$type) # number of variables
-  if(is.na(basic.drawings[1])){
-    U <- matrix(runif(nb.var*nb.sim),nb.sim,nb.var)
-  }else{
-    if(dim(basic.drawings)[1]!=nb.sim){
-      print("Dimension of basic.drawing not consistent with nb.sim")
-      return(0)
-    }else{
-      U <- basic.drawings
-    }
-  }
-
-  for(i in 1:nb.var){
+  for(i in 1:length(distri$type)){
     if(distri$type[i]=="gaussian"){
-      eps.i <- qnorm(U[,i])
+      eps.i <- rnorm(nb.sim)
     }else if(distri$type[i]=="mixt.gaussian"){
-      eps.i <- qmixt(alpha=U[,i],distri$mu[i],distri$sigma[i],distri$p[i])
+      p <- distri$p[i]
+      mu.1 <- distri$mu[i]
+      sigma.1 <- distri$sigma[i]
+      mu.2 <- - p/(1-p)*mu.1
+      sigma.2 <- sqrt( 1/(1-p) * (1 - p*sigma.1^2 - p/(1-p)*mu.1^2) )
+      B <- (runif(nb.sim)<p)*1
+      eps.i <- B*rnorm(nb.sim,mean = mu.1,sd = sigma.1) +
+        (1-B)*rnorm(nb.sim,mean = mu.2,sd = sigma.2)
     }else if(distri$type[i]=="student"){
       nu <- distri$df[i]
-      eps.i <- qt(U[,i],df = nu)/sqrt(nu/(nu-2))
+      eps.i <- rt(nb.sim,df = nu)/sqrt(nu/(nu-2))
+    }else if(distri$type[i]=="laplace"){
+      U <- runif(nb.sim) - .5
+      b <- 1/sqrt(2)
+      eps.i <- - b * sign(U) * log(1 - 2 * abs(U))
+    }else if(distri$type[i]=="hyper.sec"){
+      U <- runif(nb.sim)
+      eps.i <- 2/pi * log(tan(pi/2*U))
     }
     eps <- cbind(eps,eps.i)
   }
